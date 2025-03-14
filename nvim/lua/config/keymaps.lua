b42c1da81d065cf15a9ae85d107aa6b42632ad34
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-03-13 20:23:45 -0500

    feat: update nvim config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,18 +1,77 @@
+-- This file contains custom key mappings for Neovim.
+
 -- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua Add any additional keymaps here
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
 
 -- Position cursor at the middle of the screen after scrolling half page
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Scroll down half a page and center the cursor
+vim.keymap.set("n", "<C-u>", "<C-u>zz") -- Scroll up half a page and center the cursor
 
 -- Map Ctrl+b in insert mode to delete to the end of the word without leaving insert mode
 vim.keymap.set("i", "<C-b>", "<C-o>de")
 
--- My keymaps - TERMINAL
+-- Map Ctrl+c to escape from other modes
+vim.keymap.set({ "i", "n", "v" }, "<C-c>", [[<C-\><C-n>]])
+
+----- Tmux Navigation ------
+local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft) -- Navigate to the left pane
+vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown) -- Navigate to the bottom pane
+vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp) -- Navigate to the top pane
+vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight) -- Navigate to the right pane
+vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive) -- Navigate to the last active pane
+vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext) -- Navigate to the next pane
+
+----- OBSIDIAN -----
+vim.keymap.set(
+  "n",
+  "<leader>oc",
+  "<cmd>lua require('obsidian').util.toggle_checkbox()<CR>",
+  { desc = "Obsidian Check Checkbox" }
+)
+vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianTemplate<CR>", { desc = "Insert Obsidian Template" })
+vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<CR>", { desc = "Open in Obsidian App" })
+vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show ObsidianBacklinks" })
+vim.keymap.set("n", "<leader>ol", "<cmd>ObsidianLinks<CR>", { desc = "Show ObsidianLinks" })
+vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "Create New Note" })
+vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search Obsidian" })
+vim.keymap.set("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick Switch" })
+
+----- OIL -----
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Delete all buffers but the current one
+vim.keymap.set(
+  "n",
+  "<leader>bq",
+  '<Esc>:%bdelete|edit #|normal`"<Return>',
+  { desc = "Delete other buffers but the current one" }
+)
+
+-- Disable key mappings in insert mode
+vim.api.nvim_set_keymap("i", "<A-j>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<A-k>", "<Nop>", { noremap = true, silent = true })
+
+-- Disable key mappings in normal mode
+vim.api.nvim_set_keymap("n", "<A-j>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-k>", "<Nop>", { noremap = true, silent = true })
+
+-- Disable key mappings in visual block mode
+vim.api.nvim_set_keymap("x", "<A-j>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "<A-k>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "J", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "K", "<Nop>", { noremap = true, silent = true })
+
+-- Redefine Ctrl+s to save with the custom function
+vim.api.nvim_set_keymap("n", "<C-s>", ":lua SaveFile()<CR>", { noremap = true, silent = true })
+
+-- TERMINAL
 vim.api.nvim_set_keymap(
   "n",
   "<leader>tt",
-  "<CMD>ToggleTerm size=5 direction=float<CR>",
+  "<CMD>ToggleTerm size=1 direction=float<CR>",
   { noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap(
@@ -31,100 +90,22 @@ vim.api.nvim_set_keymap(
 ----- REST -----
 vim.keymap.set("n", "<leader>rr", ":call VrcQuery()<CR>")
 
------ Tmux Navigation ------
-local nvim_tmux_nav = require("nvim-tmux-navigation")
+-- Custom save function
+function SaveFile()
+  -- Check if a buffer with a file is open
+  if vim.fn.empty(vim.fn.expand("%:t")) == 1 then
+    vim.notify("No file to save", vim.log.levels.WARN)
+    return
+  end
 
-vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+  local filename = vim.fn.expand("%:t") -- Get only the filename
+  local success, err = pcall(function()
+    vim.cmd("silent! write") -- Try to save the file without showing the default message
+  end)
 
--- REQUIRED
-local harpoon = require("harpoon")
-local oil = require("oil")
-harpoon:setup()
-oil.setup()
--- REQUIRED
-
------ OBSIDIAN -----
-vim.keymap.set(
-  "n",
-  "<leader>oc",
-  "<cmd>lua require('obsidian').util.toggle_checkbox()<CR>",
-  { desc = "Obsidian Check Checkbox" }
-)
-vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianTemplate<CR>", { desc = "Insert Obsidian Template" })
-vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<CR>", { desc = "Open in Obsidian App" })
-vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show ObsidianBacklinks" })
-vim.keymap.set("n", "<leader>ol", "<cmd>ObsidianLinks<CR>", { desc = "Show ObsidianLinks" })
-vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "Create New Note" })
-vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search Obsidian" })
-vim.keymap.set("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick Switch" })
-vim.keymap.set("n", "gd", "<cmd>ObsidianFollowLink<CR>", { desc = "Switch follow link" })
-
------ GITHUB -----
-vim.keymap.set("i", "<M-.>", 'copilot#Accept("\\<CR>")', {
-  expr = true,
-  replace_keycodes = false,
-  silent = true,
-  noremap = true,
-})
-vim.g.copilot_no_tab_map = true
-
------  OIL -----
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
--- Delete all buffers but the current one --
-vim.keymap.set(
-  "n",
-  "<leader>bq",
-  '<Esc>:%bdelete|edit #|normal`"<Return>',
-  { desc = "Delete other buffers but the current one" }
-)
-
------ HARPOON 2 -----
-vim.keymap.set("n", "<leader>a", function()
-  harpoon:list():append()
-end, { desc = "Add harpoon mark" })
-
-vim.keymap.set("n", "<C-e>", function()
-  harpoon.ui:toggle_quick_menu(harpoon:list())
-end)
-
-vim.keymap.set("n", "<C-M-h>", function()
-  harpoon:list():select(1)
-end)
-
-vim.keymap.set("n", "<C-M-j>", function()
-  harpoon:list():select(2)
-end)
-
-vim.keymap.set("n", "<C-M-k>", function()
-  harpoon:list():select(3)
-end)
-
-vim.keymap.set("n", "<C-M-l>", function()
-  harpoon:list():select(4)
-end)
-
--- Disable key mappings in insert mode
-vim.api.nvim_set_keymap("i", "<A-j>", "<Nop>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<A-k>", "<Nop>", { noremap = true, silent = true })
-
--- Disable key mappings in normal mode
-vim.api.nvim_set_keymap("n", "<A-j>", "<Nop>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<A-k>", "<Nop>", { noremap = true, silent = true })
-
--- Disable key mappings in visual block mode
-vim.api.nvim_set_keymap("x", "<A-j>", "<Nop>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "<A-k>", "<Nop>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "J", "<Nop>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "K", "<Nop>", { noremap = true, silent = true })
-
--- Move Line
-local opts = { noremap = true, silent = true }
--- Normal mode
-vim.keymap.set("n", "<A-j>", ":MoveLine(1)<CR>", opts)
-vim.keymap.set("n", "<A-k>", ":MoveLine(-1)<CR>", opts)
+  if success then
+    vim.notify(filename .. " Saved!") -- Show only the custom message if successful
+  else
+    vim.notify("Error: " .. err, vim.log.levels.ERROR) -- Show the error message if it fails
+  end
+end
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,32 +1,96 @@
 return {
   {
+    -- Plugin for the Catppuccin color scheme
     "catppuccin/nvim",
     name = "catppuccin",
-    lazy = false,
+    lazy = false,                    -- Load this plugin immediately
     opts = {
-      transparent_background = true,
-      flavour = "mocha",
+      transparent_background = true, -- Enable transparent background
+      flavour = "mocha",             -- Set the flavor of the color scheme
     },
     integrations = {
-      cmp = true,
-      gitsigns = true,
-      nvimtree = true,
-      treesitter = true,
-      notify = false,
+      cmp = true,               -- Enable integration with nvim-cmp
+      gitsigns = true,          -- Enable integration with gitsigns
+      nvimtree = true,          -- Enable integration with nvim-tree
+      treesitter = true,        -- Enable integration with treesitter
+      notify = false,           -- Disable integration with nvim-notify
       mini = {
-        enabled = true,
-        indentscope_color = "",
+        enabled = true,         -- Enable mini plugin integration
+        indentscope_color = "", -- Set indentscope color (empty means default)
       },
-      -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+      -- Additional plugin integrations can be found in the documentation
     },
   },
   {
+    -- Plugin for the Modus Themes
     "miikanissi/modus-themes.nvim",
     name = "modus",
-    priority = 1000,
+    priority = 1000, -- High priority to ensure it loads early
   },
   {
+    -- Plugin for the Kanagawa color scheme
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    priority = 1000,      -- High priority to ensure it loads early
+    opts = {
+      transparent = true, -- Enable transparent background
+      theme = "dragon",   -- Set the theme variant to 'dragon'
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          NormalFloat = { bg = "none" },                                           -- Transparent background for floating windows
+          FloatBorder = { bg = "none" },                                           -- Transparent background for floating window borders
+          FloatTitle = { bg = "none" },                                            -- Transparent background for floating window titles
+
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },              -- Custom colors for dark mode
+
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },              -- Custom colors for Lazy plugin
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },             -- Custom colors for Mason plugin
+
+          TelescopeTitle = { fg = theme.ui.special, bold = true },                 -- Custom colors for Telescope titles
+          TelescopePromptNormal = { bg = theme.ui.bg_p1 },                         -- Custom colors for Telescope prompt
+          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },    -- Custom colors for Telescope prompt border
+          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },  -- Custom colors for Telescope results
+          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },   -- Custom colors for Telescope results border
+          TelescopePreviewNormal = { bg = theme.ui.bg_dim },                       -- Custom colors for Telescope preview
+          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim }, -- Custom colors for Telescope preview border
+
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },                   -- Custom colors for popup menu
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },                         -- Custom colors for selected item in popup menu
+          PmenuSbar = { bg = theme.ui.bg_m1 },                                     -- Custom colors for popup menu scrollbar
+          PmenuThumb = { bg = theme.ui.bg_p2 },                                    -- Custom colors for popup menu thumb
+        }
+      end,
+    },
+  },
+  -- Plugin for the Rose Pine color scheme
+  { "rose-pine/neovim",                 name = "rose-pine" },
+  -- Plugin for the Oxocarbon color scheme
+  { "nyoom-engineering/oxocarbon.nvim", name = "oxocarbon" },
+  -- Plugin for the Lush color scheme with a dependency on Zenbones
+  { "rktjmp/lush.nvim",                 dependencies = { "mcchrish/zenbones.nvim" } },
+  {
+    -- Plugin for the Cyberdream color scheme
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,    -- Load this plugin immediately
+    priority = 1000, -- High priority to ensure it loads early
+  },
+
+  -- Plugin for the Doom One color scheme
+  { "NTBBloodbath/doom-one.nvim", name = "doom-one" },
+
+  -- Plugin for the Aylin color scheme
+  { "AhmedAbdulrahman/aylin.vim", name = "aylin" },
+
+  {
+    -- Plugin for enabling transparency
     "xiyaowong/transparent.nvim",
+  },
+
+  {
+    -- Plugin for the Bamboo color scheme
+    "ribru17/bamboo.nvim",
+    lazy = false, -- Load this plugin immediately
   },
 
   {
@@ -38,24 +102,49 @@ return {
       vim.g.everforest_float_style = "dim"
     end,
   },
+
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "everforest",
-    },
+    -- Plugin for the Oldworld color scheme
+    "dgox16/oldworld.nvim",
+    lazy = false,    -- Load this plugin immediately
+    priority = 1000, -- High priority to ensure it loads early
   },
   {
-    "rebelot/kanagawa.nvim",
-    name = "kanagawa",
-    opts = {
-      transparent_background = true,
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "kanagawa-dragon",
-    },
+    "ellisonleao/gruvbox.nvim",
+    name = "gruvbox",
     priority = 1000,
+    config = true,
+    opts = {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = false,   -- invert background for search, diffs, statuslines and errors
+      contrast = "hard", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = true,
+    }
+  },
+  {
+    -- LazyVim configuration
+    "LazyVim/LazyVim",
+    opts = {
+      -- Set the default color scheme
+      colorscheme = "gruvbox",
+    },
   },
 }
